e87e0e95badef57804489684c9f38a13ece6581e
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-21 23:01:09 +0200

    Height map support implemented

--- engine/src/main/resources/shaders/fragment/basic.glsl
+++ engine/src/main/resources/shaders/fragment/basic.glsl
@@ -6,8 +6,13 @@ in vec2 vertexTextureCoordinates;
 out vec4 FragColor;
 
 uniform sampler2D textureDiffuse;
+uniform sampler2D heightTexture;
 
 void main() {
-    vec4 texColor = texture(textureDiffuse, vertexTextureCoordinates);
-    FragColor = vertexColor * texColor;
+    vec4 diffuseColor = texture(textureDiffuse, vertexTextureCoordinates);
+    float heightValue = texture(heightTexture, vertexTextureCoordinates).r;
+
+    vec3 colorMod = diffuseColor.rgb * (0.8 + 0.2 * heightValue);
+
+    FragColor = vertexColor * vec4(colorMod, diffuseColor.a);
 }
