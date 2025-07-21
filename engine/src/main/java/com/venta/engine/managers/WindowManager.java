e37b4e4025b27d806f43aed138ad9fde9f5022fd
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-21 19:12:59 +0200

    Now the app has an icon

--- engine/src/main/java/com/venta/engine/managers/WindowManager.java
+++ engine/src/main/java/com/venta/engine/managers/WindowManager.java
@@ -1,5 +1,19 @@
 package com.venta.engine.managers;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11C.glViewport;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import org.joml.Matrix4f;
+import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
+import org.lwjgl.glfw.GLFWImage;
+import org.lwjgl.stb.STBImage;
+import org.lwjgl.system.MemoryStack;
+import org.lwjgl.system.MemoryUtil;
+
 import com.venta.engine.annotations.Component;
 import com.venta.engine.configurations.WindowConfiguration;
 import com.venta.engine.exceptions.WindowCreationException;
@@ -9,13 +23,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.joml.Matrix4f;
-import org.lwjgl.glfw.GLFWFramebufferSizeCallback;
-
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11C.glViewport;
-import static org.lwjgl.system.MemoryUtil.NULL;
 
 @Slf4j
 @Component
@@ -26,27 +35,35 @@ public final class WindowManager extends AbstractManager<WindowManager.WindowEnt
 
     public WindowView create(final WindowConfiguration configuration) {
         if (!configuration.isFullscreen())
-            return create(configuration.title(), configuration.width(), configuration.height());
+            return create(configuration.title(), NULL, configuration.width(), configuration.height());
 
-        final var videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        final var monitorID = glfwGetPrimaryMonitor();
+        if (monitorID == NULL)
+            throw new WindowCreationException("Can't determine primary monitor");
+
+        final var videoMode = glfwGetVideoMode(monitorID);
         if (videoMode == null)
             throw new WindowCreationException("Can't determine video mode for the fullscreen window");
 
-        return create(configuration.title(), videoMode.width(), videoMode.height());
+        return create(configuration.title(), monitorID, videoMode.width(), videoMode.height());
     }
 
-    public WindowView create(final String title, final int width, final int height) {
+    private WindowView create(final String title, final long monitorID, final int width, final int height) {
         log.info("Creating window: {}", title);
-        final var id = glfwCreateWindow(width, height, title, NULL, NULL);
+        final var id = glfwCreateWindow(width, height, title, monitorID, NULL);
         if (id == NULL)
             throw new WindowCreationException(title);
 
         glfwMakeContextCurrent(id);
         glfwSwapInterval(1); // vertical synchronization (setting to 0 produces 5000 FPS)
         glfwShowWindow(id);
+        glfwRestoreWindow(id);
+        glfwFocusWindow(id);
 
         final var window = new WindowEntity(id, width, height, title);
         glfwSetFramebufferSizeCallback(id, window.getSizeCallback());
+
+        setIconFromResources(id, "/icons/venta.png");
 
         return store(window);
     }
@@ -54,6 +71,40 @@ public final class WindowManager extends AbstractManager<WindowManager.WindowEnt
     public void setCurrent(@NonNull final WindowView window) {
         this.current = window;
         glfwMakeContextCurrent(get(window.getId()).entity().getId());
+    }
+
+    //TODO: Reimplement it more clean (using ResourceManager)
+    @SneakyThrows
+    public void setIconFromResources(final long windowID, final String resourcePath) {
+        try (final var stack = MemoryStack.stackPush(); final var stream = getClass().getResourceAsStream(resourcePath)) {
+            if (stream == null)
+                throw new IllegalArgumentException("Resource not found: " + resourcePath);
+
+            final byte[] bytes = stream.readAllBytes();
+            final ByteBuffer imageBuffer = MemoryUtil.memAlloc(bytes.length);
+            imageBuffer.put(bytes).flip();
+
+            final IntBuffer w = stack.mallocInt(1);
+            final IntBuffer h = stack.mallocInt(1);
+            final IntBuffer comp = stack.mallocInt(1);
+
+            final ByteBuffer iconPixels = STBImage.stbi_load_from_memory(imageBuffer, w, h, comp, 4);
+            if (iconPixels == null) {
+                MemoryUtil.memFree(imageBuffer);
+                throw new RuntimeException("Failed to load icon from memory: " + STBImage.stbi_failure_reason());
+            }
+
+            final GLFWImage icon = GLFWImage.malloc(stack);
+            icon.set(w.get(0), h.get(0), iconPixels);
+
+            final GLFWImage.Buffer icons = GLFWImage.malloc(1, stack);
+            icons.put(0, icon);
+
+            glfwSetWindowIcon(windowID, icons);
+
+            STBImage.stbi_image_free(iconPixels);
+            MemoryUtil.memFree(imageBuffer);
+        }
     }
 
     @Override
