6e993f94d7a3d745dbf34f380a121f305c97e8a2
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-07-21 18:15:07 +0200

    Calculating model and view matrix once per frame

--- engine/src/main/java/com/venta/engine/renderers/AbstractRenderer.java
+++ engine/src/main/java/com/venta/engine/renderers/AbstractRenderer.java
@@ -1,20 +1,58 @@
 package com.venta.engine.renderers;
 
+import java.nio.FloatBuffer;
+
+import org.lwjgl.system.MemoryUtil;
+
 import com.venta.engine.managers.AbstractManager;
+import com.venta.engine.model.view.CameraView;
+import com.venta.engine.model.view.WindowView;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 
-public interface AbstractRenderer<E extends AbstractManager.AbstractEntity, V extends AbstractRenderer.AbstractView<E>> {
-    void render(final V view);
+public abstract class AbstractRenderer<E extends AbstractManager.AbstractEntity, V extends AbstractRenderer.AbstractView<E>> {
+    @Getter(AccessLevel.PROTECTED)
+    private RenderContext context;
+
+    abstract void render(final V view);
 
     @AllArgsConstructor
-    abstract class AbstractView<E extends AbstractManager.AbstractEntity> {
+    public abstract static class AbstractView<E extends AbstractManager.AbstractEntity> {
         @Getter
         @NonNull
         private final String id;
 
         @NonNull
         protected final E entity;
+    }
+
+    @Getter(AccessLevel.PACKAGE)
+    static final class RenderContext implements AutoCloseable {
+        private final FloatBuffer projectionMatrixBuffer;
+        private final FloatBuffer viewMatrixBuffer;
+
+        public RenderContext(final CameraView camera, final WindowView window) {
+            this.viewMatrixBuffer = MemoryUtil.memAllocFloat(16);
+            camera.entity.getViewMatrix().get(viewMatrixBuffer);
+
+            this.projectionMatrixBuffer = MemoryUtil.memAllocFloat(16);
+            window.entity.getProjectionMatrix().get(projectionMatrixBuffer);
+        }
+
+        @Override
+        public void close() {
+            MemoryUtil.memFree(projectionMatrixBuffer);
+            MemoryUtil.memFree(viewMatrixBuffer);
+        }
+    }
+
+    final AutoCloseable withContext(@NonNull final RenderContext context) {
+        this.context = context;
+        return () -> {
+            this.context = null;
+            context.close();
+        };
     }
 }
