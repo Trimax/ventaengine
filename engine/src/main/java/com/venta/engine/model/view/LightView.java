02c1c02b69cee230d6ce206210608a37e4ae8a5f
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-07-22 14:03:45 +0200

    Light manager introduced. Basic light configuration added

---
+++ engine/src/main/java/com/venta/engine/model/view/LightView.java
@@ -0,0 +1,35 @@
+package com.venta.engine.model.view;
+
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import com.venta.engine.managers.LightManager;
+import com.venta.engine.renderers.AbstractRenderer;
+import lombok.Getter;
+
+public final class LightView extends AbstractRenderer.AbstractView<LightManager.LightEntity> {
+    @Getter
+    private final Vector3f position = new Vector3f(0.f, 0.f, 0.f);
+
+    @Getter
+    private final Vector3f direction = new Vector3f(0.f, 0.f, 0.f);
+
+    @Getter
+    private final Vector4f color = new Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
+
+    public LightView(final String id, final LightManager.LightEntity entity) {
+        super(id, entity);
+    }
+
+    public void setPosition(final Vector3f position) {
+        this.position.set(position);
+    }
+
+    public void setDirection(final Vector3f position) {
+        this.direction.set(position);
+    }
+
+    public void setColor(final Vector4f color) {
+        this.color.set(color);
+    }
+}
