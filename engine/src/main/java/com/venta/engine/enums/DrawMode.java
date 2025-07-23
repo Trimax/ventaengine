0881c98ed7e53e75248e02769cd850bb3c7eae9a
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-23 21:04:50 +0200

    Draw mode support added

---
+++ engine/src/main/java/com/venta/engine/enums/DrawMode.java
@@ -0,0 +1,17 @@
+package com.venta.engine.enums;
+
+import static org.lwjgl.opengl.GL11C.*;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum DrawMode {
+    Polygon(GL_FILL),
+    Edge(GL_LINE),
+    Vertex(GL_POINT);
+
+    private final int mode;
+}
