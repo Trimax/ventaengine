23b7a989b7130faad13e2220af8bf73d9680e4cc
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-25 12:56:55 +0200

    Improving TBN computation (clean code)

--- engine/src/main/java/com/venta/engine/model/dto/ObjectDTO.java
+++ engine/src/main/java/com/venta/engine/model/dto/ObjectDTO.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.tuple.Triple;
 import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
@@ -35,17 +37,15 @@ public record ObjectDTO(String type,
     }
 
     public float[] getVerticesArray() {
-        final var normals = computeVertexNormals();
-        final var tangents = computeVertexTangents();
-        final var bitangents = computeVertexBitangents();
+        final var tbnVectors = computeTBNVectors();
 
         final var packedArray = new float[vertices.size() * 18];
         for (int vertexID = 0; vertexID < vertices.size(); vertexID++) {
             final var vertex = vertices.get(vertexID);
 
-            final var normal = normals.get(vertexID);
-            final var tangent = tangents.get(vertexID);
-            final var bitangent = bitangents.get(vertexID);
+            final var normal = Optional.ofNullable(tbnVectors.get(vertexID)).map(Triple::getLeft).orElse(null);
+            final var tangent = Optional.ofNullable(tbnVectors.get(vertexID)).map(Triple::getMiddle).orElse(null);
+            final var bitangent = Optional.ofNullable(tbnVectors.get(vertexID)).map(Triple::getRight).orElse(null);
 
             if (vertex.hasPosition()) {
                 packedArray[18 * vertexID] = vertex.position().x();
@@ -53,16 +53,16 @@ public record ObjectDTO(String type,
                 packedArray[18 * vertexID + 2] = vertex.position().z();
             }
 
-            if (normal != null) {
-                packedArray[18 * vertexID + 3] = normal.x();
-                packedArray[18 * vertexID + 4] = normal.y();
-                packedArray[18 * vertexID + 5] = normal.z();
-            }
-
             if (vertex.hasNormal()) {
                 packedArray[18 * vertexID + 3] = vertex.normal.x();
                 packedArray[18 * vertexID + 4] = vertex.normal.y();
                 packedArray[18 * vertexID + 5] = vertex.normal.z();
+            }
+
+            if (normal != null) {
+                packedArray[18 * vertexID + 3] = normal.x();
+                packedArray[18 * vertexID + 4] = normal.y();
+                packedArray[18 * vertexID + 5] = normal.z();
             }
 
             if (vertex.hasTextureCoordinates()) {
@@ -145,7 +145,6 @@ public record ObjectDTO(String type,
             final var vertex2 = vertices.get(facet.vertex2()).position();
             final var vertex3 = vertices.get(facet.vertex3()).position();
 
-
             final var edge1 = vertex2.sub(vertex1, new Vector3f());
             final var edge2 = vertex3.sub(vertex1, new Vector3f());
 
@@ -155,122 +154,116 @@ public record ObjectDTO(String type,
         return faceNormals;
     }
 
-    private Map<Integer, Vector3f> computeVertexNormals() {
+    private Map<Integer, Triple<Vector3f, Vector3f, Vector3f>> computeTBNVectors() {
         if (!hasFacets())
             return new HashMap<>();
 
         final var faceNormals = computeFaceNormals();
 
         final var vertexNormals = new HashMap<Integer, List<Vector3f>>();
+        final var vertexTangents = new HashMap<Integer, List<Vector3f>>();
+        final var vertexBitangents = new HashMap<Integer, List<Vector3f>>();
+
         for (int facetIndex = 0; facetIndex < facets.size(); facetIndex++) {
             final var facet = facets.get(facetIndex);
-            final var faceNormal = faceNormals.get(facetIndex);
 
-            add(vertexNormals, faceNormal, facet.vertex1);
-            add(vertexNormals, faceNormal, facet.vertex2);
-            add(vertexNormals, faceNormal, facet.vertex3);
-        }
+            final Vertex vertex0 = vertices.get(facet.vertex1());
+            final Vertex vertex1 = vertices.get(facet.vertex2());
+            final Vertex vertex2 = vertices.get(facet.vertex3());
 
-        return EntryStream.of(vertexNormals).mapValues(this::merge).toMap();
-    }
+            final var normal = faceNormals.get(facetIndex);
+            add(vertexNormals, normal, facet.vertex1);
+            add(vertexNormals, normal, facet.vertex2);
+            add(vertexNormals, normal, facet.vertex3);
 
-    private Map<Integer, Vector3f> computeVertexTangents() {
-        if (!hasFacets())
-            return new HashMap<>();
+            if (!hasTextureCoordinates(vertex0, vertex1, vertex2))
+                continue;
 
-        final Map<Integer, List<Vector3f>> vertexTangents = new HashMap<>();
-        for (final Facet facet : facets) {
-            final Vertex v0 = vertices.get(facet.vertex1());
-            final Vertex v1 = vertices.get(facet.vertex2());
-            final Vertex v2 = vertices.get(facet.vertex3());
-
-            final Vector3f pos1 = v0.position();
-            final Vector3f pos2 = v1.position();
-            final Vector3f pos3 = v2.position();
-
-            final var uv1 = new Vector2f(v0.textureCoordinates().x(), v0.textureCoordinates().y());
-            final var uv2 = new Vector2f(v1.textureCoordinates().x(), v1.textureCoordinates().y());
-            final var uv3 = new Vector2f(v2.textureCoordinates().x(), v2.textureCoordinates().y());
-
-            final var edge1 = pos2.sub(pos1, new Vector3f());
-            final var edge2 = pos3.sub(pos1, new Vector3f());
-
-            final float deltaU1 = uv2.x - uv1.x;
-            final float deltaV1 = uv2.y - uv1.y;
-            final float deltaU2 = uv3.x - uv1.x;
-            final float deltaV2 = uv3.y - uv1.y;
-
-            float f = deltaU1 * deltaV2 - deltaU2 * deltaV1;
-            if (f == 0.0f)
-                f = 1.0f;
-            else
-                f = 1.0f / f;
-
-            final var tangent = new Vector3f(
-                    f * (deltaV2 * edge1.x() - deltaV1 * edge2.x()),
-                    f * (deltaV2 * edge1.y() - deltaV1 * edge2.y()),
-                    f * (deltaV2 * edge1.z() - deltaV1 * edge2.z())
-            );
-
+            final var tangent = computeTangent(vertex0, vertex1, vertex2);
             add(vertexTangents, tangent, facet.vertex1);
             add(vertexTangents, tangent, facet.vertex2);
             add(vertexTangents, tangent, facet.vertex3);
+
+            final var bitangent = computeBitangent(vertex0, vertex1, vertex2);
+            add(vertexBitangents, bitangent, facet.vertex1);
+            add(vertexBitangents, bitangent, facet.vertex2);
+            add(vertexBitangents, bitangent, facet.vertex3);
         }
 
-        return EntryStream.of(vertexTangents).mapValues(this::merge).toMap();
+        final var normals = EntryStream.of(vertexNormals).mapValues(this::merge).toMap();
+        final var tangents = EntryStream.of(vertexTangents).mapValues(this::merge).toMap();
+        final var bitangents = EntryStream.of(vertexBitangents).mapValues(this::merge).toMap();
+
+        final var tbnVectors = new HashMap<Integer, Triple<Vector3f, Vector3f, Vector3f>>();
+        for (int vertexID = 0; vertexID < vertices.size(); vertexID++)
+            tbnVectors.put(vertexID, Triple.of(normals.get(vertexID), tangents.get(vertexID), bitangents.get(vertexID)));
+
+        return tbnVectors;
     }
 
-    private Map<Integer, Vector3f> computeVertexBitangents() {
-        if (!hasFacets())
-            return new HashMap<>();
+    private Vector3f computeTangent(final Vertex vertex0, final Vertex vertex1, final Vertex vertex2) {
+        final Vector3f pos1 = vertex0.position();
+        final Vector3f pos2 = vertex1.position();
+        final Vector3f pos3 = vertex2.position();
 
-        final Map<Integer, List<Vector3f>> vertexBitangents = new HashMap<>();
-        for (final Facet facet : facets) {
-            final Vertex v0 = vertices.get(facet.vertex1());
-            final Vertex v1 = vertices.get(facet.vertex2());
-            final Vertex v2 = vertices.get(facet.vertex3());
+        final var uv1 = new Vector2f(vertex0.textureCoordinates().x(), vertex0.textureCoordinates().y());
+        final var uv2 = new Vector2f(vertex1.textureCoordinates().x(), vertex1.textureCoordinates().y());
+        final var uv3 = new Vector2f(vertex2.textureCoordinates().x(), vertex2.textureCoordinates().y());
 
-            final Vector3f pos1 = v0.position();
-            final Vector3f pos2 = v1.position();
-            final Vector3f pos3 = v2.position();
+        final var edge1 = pos2.sub(pos1, new Vector3f());
+        final var edge2 = pos3.sub(pos1, new Vector3f());
 
-            final Vector2f uv1 = new Vector2f(v0.textureCoordinates().x(), v0.textureCoordinates().y());
-            final Vector2f uv2 = new Vector2f(v1.textureCoordinates().x(), v1.textureCoordinates().y());
-            final Vector2f uv3 = new Vector2f(v2.textureCoordinates().x(), v2.textureCoordinates().y());
+        final float deltaU1 = uv2.x - uv1.x;
+        final float deltaV1 = uv2.y - uv1.y;
+        final float deltaU2 = uv3.x - uv1.x;
+        final float deltaV2 = uv3.y - uv1.y;
 
-            final Vector3f edge1 = pos2.sub(pos1, new Vector3f());
-            final Vector3f edge2 = pos3.sub(pos1, new Vector3f());
-
-            final float deltaU1 = uv2.x - uv1.x;
-            final float deltaV1 = uv2.y - uv1.y;
-            final float deltaU2 = uv3.x - uv1.x;
-            final float deltaV2 = uv3.y - uv1.y;
-
-            float f = deltaU1 * deltaV2 - deltaU2 * deltaV1;
-            if (f == 0.0f)
-                f = 1.0f;
-            else
-                f = 1.0f / f;
-
-            final Vector3f bitangent = new Vector3f(
-                    f * (-deltaU2 * edge1.x() + deltaU1 * edge2.x()),
-                    f * (-deltaU2 * edge1.y() + deltaU1 * edge2.y()),
-                    f * (-deltaU2 * edge1.z() + deltaU1 * edge2.z())
-            );
-
-            add(vertexBitangents, bitangent, facet.vertex1());
-            add(vertexBitangents, bitangent, facet.vertex2());
-            add(vertexBitangents, bitangent, facet.vertex3());
-        }
-
-        return EntryStream.of(vertexBitangents).mapValues(this::merge).toMap();
+        float f = safeInverse(deltaU1 * deltaV2 - deltaU2 * deltaV1);
+        return new Vector3f(
+                f * (deltaV2 * edge1.x() - deltaV1 * edge2.x()),
+                f * (deltaV2 * edge1.y() - deltaV1 * edge2.y()),
+                f * (deltaV2 * edge1.z() - deltaV1 * edge2.z())
+        );
     }
 
-    private Vector3f merge(final List<Vector3f> normals) {
-        final var mergedNormal = new Vector3f();
-        StreamEx.of(normals).forEach(mergedNormal::add);
+    private Vector3f computeBitangent(final Vertex vertex0, final Vertex vertex1, final Vertex vertex2) {
+        final Vector3f pos1 = vertex0.position();
+        final Vector3f pos2 = vertex1.position();
+        final Vector3f pos3 = vertex2.position();
 
-        return mergedNormal.normalize();
+        final Vector2f uv1 = new Vector2f(vertex0.textureCoordinates().x(), vertex0.textureCoordinates().y());
+        final Vector2f uv2 = new Vector2f(vertex1.textureCoordinates().x(), vertex1.textureCoordinates().y());
+        final Vector2f uv3 = new Vector2f(vertex2.textureCoordinates().x(), vertex2.textureCoordinates().y());
+
+        final Vector3f edge1 = pos2.sub(pos1, new Vector3f());
+        final Vector3f edge2 = pos3.sub(pos1, new Vector3f());
+
+        final float deltaU1 = uv2.x - uv1.x;
+        final float deltaV1 = uv2.y - uv1.y;
+        final float deltaU2 = uv3.x - uv1.x;
+        final float deltaV2 = uv3.y - uv1.y;
+
+        float f = safeInverse(deltaU1 * deltaV2 - deltaU2 * deltaV1);
+        return new Vector3f(
+                f * (-deltaU2 * edge1.x() + deltaU1 * edge2.x()),
+                f * (-deltaU2 * edge1.y() + deltaU1 * edge2.y()),
+                f * (-deltaU2 * edge1.z() + deltaU1 * edge2.z())
+        );
+    }
+
+    private boolean hasTextureCoordinates(final Vertex... vertices) {
+        return StreamEx.of(vertices).allMatch(Vertex::hasTextureCoordinates);
+    }
+
+    private float safeInverse(final float value) {
+        return (value == 0.0f) ? 1.0f : 1.0f / value;
+    }
+
+    private Vector3f merge(final List<Vector3f> vectors) {
+        final var sum = new Vector3f();
+        StreamEx.of(vectors).forEach(sum::add);
+
+        return sum.normalize();
     }
 
     private void add(final Map<Integer, List<Vector3f>> vertexNormals, final Vector3f normal, final int vertexID) {
