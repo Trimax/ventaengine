4217fb0a6330f0347cfa58b072894915ebe97aef
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-07-25 14:08:13 +0200

    Introducing definitions and small cleanup (#13)

--- engine/src/test/java/com/venta/engine/model/ObjectDTOTest.java
+++ engine/src/test/java/com/venta/engine/model/ObjectDTOTest.java
@@ -1,5 +1,6 @@
 package com.venta.engine.model;
 
+import static com.venta.engine.definitions.Definitions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -17,39 +18,31 @@ public final class ObjectDTOTest {
 
         final var verticesArray = triangle.getVerticesArray();
         assertNotNull(verticesArray, "Vertices array should not be null");
-        assertEquals(18 * triangle.vertices().size(), verticesArray.length, "The number of vertices must be correct");
+        assertEquals(COUNT_FLOATS_PER_VERTEX * triangle.vertices().size(), verticesArray.length, "The number of vertices must be correct");
 
         /* Checking backed vertices positions */
-        assertEquals(0f, verticesArray[0], 1e-6, "Vertex 1 position x must be correct");
-        assertEquals(0f, verticesArray[1], 1e-6, "Vertex 1 position y must be correct");
-        assertEquals(0f, verticesArray[2], 1e-6, "Vertex 1 position z must be correct");
-
-        assertEquals(1f, verticesArray[18], 1e-6, "Vertex 2 position x must be correct");
-        assertEquals(0f, verticesArray[19], 1e-6, "Vertex 2 position x must be correct");
-        assertEquals(0f, verticesArray[20], 1e-6, "Vertex 2 position x must be correct");
-
-        assertEquals(0f, verticesArray[36], 1e-6, "Vertex 3 position x must be correct");
-        assertEquals(1f, verticesArray[37], 1e-6, "Vertex 3 position x must be correct");
-        assertEquals(0f, verticesArray[38], 1e-6, "Vertex 3 position x must be correct");
+        verifyPosition(0, verticesArray, new Vector3f(0.f, 0.f, 0.f));
+        verifyPosition(1, verticesArray, new Vector3f(1.f, 0.f, 0.f));
+        verifyPosition(2, verticesArray, new Vector3f(0.f, 1.f, 0.f));
 
         /* Checking backed normals */
         for (int i = 0; i < triangle.vertices().size(); i++) {
-            assertEquals(0f, verticesArray[i * 18 + 3], 1e-5, "Vertex " + i + " normal x must be correct");
-            assertEquals(0f, verticesArray[i * 18 + 4], 1e-5, "Vertex " + i + " normal y must be correct");
-            assertEquals(1f, verticesArray[i * 18 + 5], 1e-5, "Vertex " + i + " normal z must be correct");
+            assertEquals(0f, verticesArray[i * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_NORMAL_X], 1e-5, "Vertex " + i + " normal x must be correct");
+            assertEquals(0f, verticesArray[i * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_NORMAL_Y], 1e-5, "Vertex " + i + " normal y must be correct");
+            assertEquals(1f, verticesArray[i * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_NORMAL_Z], 1e-5, "Vertex " + i + " normal z must be correct");
         }
 
         /* Checking backed tangents */
         for (int i = 0; i < triangle.vertices().size(); i++)
-            assertTrue(verticesArray[i * 18 + 12] > 0, "Vertex " + i + " tangent direction should be correct");
+            assertTrue(verticesArray[i * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_TANGENT_X] > 0, "Vertex " + i + " tangent direction should be correct");
 
         /* Checking backed bitangents */
         for (int i = 0; i < triangle.vertices().size(); i++)
-            assertTrue(verticesArray[i * 18 + 16] > 0, "Vertex " + i + " bitangent direction should be correct");
+            assertTrue(verticesArray[i * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_BITANGENT_Y] > 0, "Vertex " + i + " bitangent direction should be correct");
 
         final var facesArray = triangle.getFacesArray();
         assertNotNull(verticesArray, "Faces array should not be null");
-        assertEquals(3 * triangle.facets().size(), facesArray.length, "The number of faces must be correct");
+        assertEquals(COUNT_VERTICES_PER_FACET * triangle.facets().size(), facesArray.length, "The number of faces must be correct");
 
         for (int i = 0; i < triangle.facets().size(); i++)
             assertEquals(i, facesArray[i], "Face vertex " + i + " index must be correct");
@@ -57,6 +50,16 @@ public final class ObjectDTOTest {
         final var edgesArray = triangle.getEdgesArray();
         assertNotNull(edgesArray, "Edges array should not be null");
         assertEquals(0, edgesArray.length, "The number of edges must be correct");
+    }
+
+    private void verifyPosition(final int vertexID, final float[] verticesArray, final Vector3f expected) {
+        verifyValue(vertexID, verticesArray[vertexID * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_POSITION_X], expected.x);
+        verifyValue(vertexID, verticesArray[vertexID * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_POSITION_Y], expected.y);
+        verifyValue(vertexID, verticesArray[vertexID * COUNT_FLOATS_PER_VERTEX + VERTEX_OFFSET_POSITION_Z], expected.z);
+    }
+
+    private void verifyValue(final int vertexID, final float actual, final float expected) {
+        assertEquals(expected, actual, 1e-6, "Vertex " + vertexID + " position z must be correct");
     }
 
     private static ObjectDTO createTriangleDTO() {
