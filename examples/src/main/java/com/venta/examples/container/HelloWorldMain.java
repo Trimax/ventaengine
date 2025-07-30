b7829132cc1dd2fba5cb5faa14ceadd10953f3b6
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-30 17:16:15 +0200

    Simplifying demo application

--- examples/src/main/java/com/venta/examples/container/HelloWorldMain.java
+++
@@ -1,9 +0,0 @@
-package com.venta.examples.container;
-
-import com.venta.container.VentaApplication;
-
-public class HelloWorldMain {
-    public static void main(final String[] args) {
-        VentaApplication.run(args, HelloWorldApp.class);
-    }
-}
