f8ad64938180ea5858c536af3425090a1ede2fbb
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-07-31 14:12:25 +0200

    Bounding boxes

---
+++ examples/src/main/java/io/github/trimax/examples/debug/DebugVentaApplication.java
@@ -0,0 +1,41 @@
+package io.github.trimax.examples.debug;
+
+import io.github.trimax.examples.debug.handlers.DebugApplicationStartupHandler;
+import io.github.trimax.examples.debug.handlers.DebugApplicationUpdateHandler;
+import io.github.trimax.examples.debug.state.DebugApplicationState;
+import io.github.trimax.venta.engine.VentaEngine;
+import io.github.trimax.venta.engine.interfaces.VentaEngineApplication;
+import io.github.trimax.venta.engine.interfaces.VentaEngineConfiguration;
+import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
+import io.github.trimax.venta.engine.interfaces.VentaEngineUpdateHandler;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public final class DebugVentaApplication implements VentaEngineApplication {
+    private final DebugApplicationState state = new DebugApplicationState();
+
+    @Override
+    public @NonNull VentaEngineConfiguration getConfiguration() {
+        return new VentaEngineConfiguration() {
+            @Override
+            public @NonNull RenderConfiguration getRenderConfiguration() {
+                return new RenderConfiguration(false, true);
+            }
+        };
+    }
+
+    @Override
+    public @NonNull VentaEngineStartupHandler getStartupHandler() {
+        return new DebugApplicationStartupHandler(state);
+    }
+
+    @Override
+    public @NonNull VentaEngineUpdateHandler getUpdateHandler() {
+        return new DebugApplicationUpdateHandler(state);
+    }
+
+    public static void main(final String[] args) {
+        VentaEngine.run(args, new DebugVentaApplication());
+    }
+}
---
+++ examples/src/main/java/io/github/trimax/examples/debug/state/DebugApplicationState.java
@@ -0,0 +1,9 @@
+package io.github.trimax.examples.debug.state;
+
+import io.github.trimax.venta.engine.model.view.ObjectView;
+import lombok.Data;
+
+@Data
+public final class DebugApplicationState {
+    private ObjectView tetrahedron;
+}
