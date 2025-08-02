fb9f0e337f502e169635182eb86d53bd89c24b22
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-08-02 10:42:13 +0200

    Time command executors were added

---
+++ engine/src/main/java/io/github/trimax/venta/engine/executors/engine/EngineTimeExecutor.java
@@ -0,0 +1,22 @@
+package io.github.trimax.venta.engine.executors.engine;
+
+import io.github.trimax.venta.container.annotations.Component;
+import io.github.trimax.venta.engine.console.ConsoleQueue;
+import io.github.trimax.venta.engine.core.InternalVentaContext;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public final class EngineTimeExecutor extends AbstractEngineExecutor {
+    private final long timeStarted = System.nanoTime();
+
+    private EngineTimeExecutor(@NonNull final InternalVentaContext context) {
+        super(context, "time", "shows the number of seconds the engine is running");
+    }
+
+    @Override
+    public void execute(final ConsoleQueue.Command command) {
+        getConsole().info(String.format("Engine works for %.3fs", (System.nanoTime() - timeStarted) / 1e9));
+    }
+}
