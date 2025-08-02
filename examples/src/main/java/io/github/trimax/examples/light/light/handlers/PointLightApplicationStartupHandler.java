4a7f0de34f394521bc09da89863b1b1e99541802
Vasili Zvarych <vasilii.zvarich@gmail.com>
2025-08-02 20:02:27 +0200

    VE-24: Scene with a plane (#40)
    Co-authored-by: Vasili Zvarych <vasili.zvarych@bcdtravel.com>

---
+++ examples/src/main/java/io/github/trimax/examples/light/light/handlers/PointLightApplicationStartupHandler.java
@@ -0,0 +1,41 @@
+package io.github.trimax.examples.light.light.handlers;
+
+import io.github.trimax.venta.engine.context.VentaContext;
+import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.joml.Vector3f;
+
+@Slf4j
+@AllArgsConstructor
+public final class PointLightApplicationStartupHandler implements VentaEngineStartupHandler {
+
+    @Override
+    public void onStartup(final String[] args, final VentaContext context) {
+        final var scene = context.getSceneManager().getCurrent();
+
+        final var plane = context.getObjectManager().load("plane");
+        plane.getMesh().setMaterial(context.getMaterialManager().load("stone"));;
+        plane.setScale(new Vector3f(50f));
+        scene.add(plane);
+
+        final var redLight = context.getLightManager().load("basic");
+        redLight.setPosition(new Vector3f(3.f, 1.f, 0.f));
+        redLight.setColor(new Vector3f(1f,0f,0f));
+        scene.add(redLight);
+
+        final var blueLight = context.getLightManager().load("basic");
+        blueLight.setPosition(new Vector3f(0.f, 1.f, 3.f));
+        blueLight.setColor(new Vector3f(0f,0f,1f));
+        scene.add(blueLight);
+
+        final var greenLight = context.getLightManager().load("basic");
+        greenLight.setPosition(new Vector3f(0.f, 1.f, 0.f));
+        greenLight.setColor(new Vector3f(0f,1f,0f));
+        scene.add(greenLight);
+
+        final var camera = context.getCameraManager().getCurrent();
+        camera.setPosition(new Vector3f(15.f, 15.f, 15.f));
+        camera.lookAt(new Vector3f(0.f));
+    }
+}
