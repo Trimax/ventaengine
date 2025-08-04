99f833c72540c79bad9d74f3de45d87400ec1179
Vasili Zvarych <vasilii.zvarich@gmail.com>
2025-08-04 13:49:09 +0200

    VE-24: small refactoring + textured cube (#44)
    Co-authored-by: Vasili Zvarych <vasili.zvarych@bcdtravel.com>

--- examples/src/main/java/io/github/trimax/examples/container/ContainerVentaApplication.java
+++ examples/src/main/java/io/github/trimax/examples/container/ContainerVentaApplication.java
@@ -7,13 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public final class DemoVentaApplication implements AbstractVentaApplication<Void> {
+public final class ContainerVentaApplication implements AbstractVentaApplication<Void> {
     @Override
     public void start(final String[] args, final Void argument) {
         log.info("Hello world from Venta!");
     }
 
     public static void main(final String[] args) {
-        VentaApplication.run(args, DemoVentaApplication.class);
+        VentaApplication.run(args, ContainerVentaApplication.class);
     }
 }
---
+++ examples/src/main/java/io/github/trimax/examples/material/texture/handlers/TexturedCubeApplicationStartupHandler.java
@@ -0,0 +1,26 @@
+package io.github.trimax.examples.material.texture.handlers;
+
+import io.github.trimax.venta.engine.context.VentaContext;
+import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
+import lombok.AllArgsConstructor;
+import org.joml.Vector3f;
+
+@AllArgsConstructor
+public final class TexturedCubeApplicationStartupHandler implements VentaEngineStartupHandler {
+    public void onStartup(final String[] args, final VentaContext context) {
+        final var scene = context.getSceneManager().getCurrent();
+
+        final var cube = context.getObjectManager().load("colouredcube");
+        cube.setScale(new Vector3f(5.f));
+        scene.add(cube);
+
+        final var lightXZ = context.getLightManager().load("basic");
+        lightXZ.setPosition(new Vector3f(3.f, 3.f, 3.f));
+        lightXZ.setIntensity(6.0f);
+        scene.add(lightXZ);
+
+        final var camera = context.getCameraManager().getCurrent();
+        camera.setPosition(new Vector3f(5.f, 5.f, 5.f));
+        camera.lookAt(new Vector3f(0.f));
+    }
+}
