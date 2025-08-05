9c3595412d70be85016ae15b5dd7f13b103de5be
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-08-05 16:09:37 +0200

    Factory refactoring

--- engine/src/main/java/io/github/trimax/venta/engine/factories/AbstractFactory.java
+++ engine/src/main/java/io/github/trimax/venta/engine/factories/AbstractFactory.java
@@ -1,5 +1,29 @@
 package io.github.trimax.venta.engine.factories;
 
-public abstract class AbstractFactory {
+import io.github.trimax.venta.engine.utils.TransformationUtil;
+import lombok.NonNull;
+import one.util.streamex.StreamEx;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+public abstract class AbstractFactory<T> {
+    private final Map<Class<?>, T> instances;
+    private final Consumer<T> cleaner;
+
+    protected AbstractFactory(@NonNull final List<T> instances, @NonNull final Consumer<T> cleaner) {
+        this.instances = TransformationUtil.toMap(instances, T::getClass);
+        this.cleaner = cleaner;
+    }
+
+    public void cleanup() {
+        StreamEx.ofValues(instances).forEach(cleaner);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C extends T> C get(@NonNull final Class<C> instanceClass) {
+        return (C) instances.get(instanceClass);
+    }
 }
+
