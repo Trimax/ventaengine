9acb2e1842b3660604d380db4507cb49afb2f48e
Vasili Zvarych <vasili.zvarych@bcdtravel.com>
2025-08-10 14:10:59 +0200

    VE-45: after code review

--- examples/src/main/java/io/github/trimax/examples/maze/handlers/MazeApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/maze/handlers/MazeApplicationStartupHandler.java
@@ -4,6 +4,7 @@ import io.github.trimax.examples.maze.state.MazeApplicationState;
 import io.github.trimax.venta.engine.context.VentaContext;
 import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
 import io.github.trimax.venta.engine.model.instance.LightInstance;
+import io.github.trimax.venta.engine.model.instance.SceneInstance;
 import io.github.trimax.venta.engine.utils.ResourceUtil;
 import lombok.AllArgsConstructor;
 import org.joml.Vector3f;
@@ -11,13 +12,12 @@ import org.joml.Vector4f;
 
 @AllArgsConstructor
 public final class MazeApplicationStartupHandler implements VentaEngineStartupHandler {
+    private static final int CUBE_SIZE = 20;
     private final MazeApplicationState state;
 
     public void onStartup(final String[] args, final VentaContext context) {
         final var scene = context.getSceneManager().getCurrent();
         scene.setAmbientLight(new Vector4f(0.2f, 0.2f, 0.2f, 1.f));
-
-        final String noNewLines = ResourceUtil.loadAsString("/maze/maze.txt").replaceAll("\\R", "");
 
         final var camera = context.getCameraManager().getCurrent();
         camera.setPosition(new Vector3f(-1.f, 0.5f, 10.f));
@@ -30,16 +30,42 @@ public final class MazeApplicationStartupHandler implements VentaEngineStartupHa
         state.setLight(light);
         scene.add(light);
 
-        final int cubeCount = 20;
-        for (int x = 0; x < cubeCount; x++) {
-            for (int z = 0; z < cubeCount; z++) {
-                if (noNewLines.charAt(x * cubeCount + z) == '0')
-                    continue;
-                final var miniCube = context.getObjectManager().create("cube" + (x * cubeCount + z),
-                        context.getObjectRepository().get("cube"));
-                miniCube.setPosition(new Vector3f(x + 0.5f, 0.5f, z + 0.5f));
-                scene.add(miniCube);
+        final Maze maze = new Maze(ResourceUtil.loadAsString("/maze/maze.txt"), CUBE_SIZE, CUBE_SIZE);
+        for (int x = 0; x < CUBE_SIZE; x++) {
+            for (int z = 0; z < CUBE_SIZE; z++) {
+                if (maze.hasCube(x, z))
+                    placeCube(x, z, context, scene);
             }
+        }
+    }
+
+    public void placeCube(final int x, final int z, final VentaContext context, final SceneInstance scene) {
+        final var miniCube = context.getObjectManager().create("cube" + (x * CUBE_SIZE + z),
+                context.getObjectRepository().get("cube"));
+        miniCube.setPosition(new Vector3f(x + 0.5f, 0.5f, z + 0.5f));
+        scene.add(miniCube);
+    }
+
+    static class Maze {
+        private final String value;
+        private final int width;
+        private final int length;
+
+        public Maze(final String value, final int width, final int length) {
+            this.value = value.replaceAll("\\R", "");
+            this.width = width;
+            this.length = length;
+
+            if (this.value.length() != width * length)
+                throw new IllegalArgumentException("The length of the value must equal to " + width * length);
+        }
+
+        public boolean hasCube(final int x, final int z) {
+            return withinBounds(x, width) && withinBounds(z, length) && value.charAt(x * length + z) == '1';
+        }
+
+        private boolean withinBounds(final int value, final int max) {
+            return value >= 0 && value < max;
         }
     }
 }
