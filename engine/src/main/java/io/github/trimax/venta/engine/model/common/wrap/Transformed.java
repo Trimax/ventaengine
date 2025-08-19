89f7dfa13102024c23378d0c83cd37ecfbfdff3c
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-08-19 10:23:08 +0200

    Changing version bump to mvn versions

--- engine/src/main/java/io/github/trimax/venta/engine/model/common/wrap/Transformed.java
+++
@@ -1,7 +0,0 @@
-package io.github.trimax.venta.engine.model.common.wrap;
-
-import io.github.trimax.venta.engine.model.common.math.Transform;
-import lombok.NonNull;
-
-public record Transformed<T>(@NonNull T value, @NonNull Transform transform) {
-}
