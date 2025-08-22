f07614a8a6e0b91e587163fb2ae2bc2d4d94346d
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-08-22 15:10:49 +0200

    Building right path when loading from the archive

--- engine/src/main/java/io/github/trimax/venta/engine/utils/ArchiveUtil.java
+++ engine/src/main/java/io/github/trimax/venta/engine/utils/ArchiveUtil.java
@@ -16,22 +16,22 @@ public final class ArchiveUtil {
     @SneakyThrows
     public void load(@NonNull final File file, @NonNull final BiConsumer<String, byte[]> resourceLoader) {
         try (var in = new DataInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(file))))) {
-            loadNode(in, resourceLoader);
+            loadNode(in, resourceLoader, "");
         }
     }
 
     @SneakyThrows
-    private void loadNode(final DataInputStream in, final BiConsumer<String, byte[]> resourceLoader) {
+    private void loadNode(final DataInputStream in, final BiConsumer<String, byte[]> resourceLoader, @NonNull final String path) {
         final var name = in.readUTF();
 
         final var hasReference = in.readBoolean();
         if (hasReference) {
             final var length = in.readInt();
-            resourceLoader.accept(name, in.readNBytes(length));
+            resourceLoader.accept(String.format("%s/%s", path, name).substring("/Root".length()), in.readNBytes(length));
         }
 
         final var childrenCount = in.readInt();
         for (int i = 0; i < childrenCount; i++)
-            loadNode(in, resourceLoader);
+            loadNode(in, resourceLoader, String.format("%s/%s", path, name));
     }
 }
