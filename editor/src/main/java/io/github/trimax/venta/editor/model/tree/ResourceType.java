fa12c87ca8670533e0f0e8f5dd83a2c3f7556ac8
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-08-24 19:47:53 +0200

    Displaying different resources in Editor

---
+++ editor/src/main/java/io/github/trimax/venta/editor/model/tree/ResourceType.java
@@ -0,0 +1,20 @@
+package io.github.trimax.venta.editor.model.tree;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ResourceType {
+    Materials("Material"),
+    Textures("Texture"),
+    Programs("Program"),
+    Shaders("Shader"),
+    Objects("Object"),
+    Scenes("Scene"),
+    Lights("Light"),
+    Meshes("Mesh");
+
+    private final String displayName;
+}
