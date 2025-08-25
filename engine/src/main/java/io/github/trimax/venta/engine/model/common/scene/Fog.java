a5914af22089fcf8c2ded3b94ff51c5fc4613089
Vasili Zvarych <vasili.zvarych@bcdtravel.com>
2025-08-25 19:41:10 +0200

    VE-37: refactoring 3

--- engine/src/main/java/io/github/trimax/venta/engine/model/common/scene/Fog.java
+++ engine/src/main/java/io/github/trimax/venta/engine/model/common/scene/Fog.java
@@ -2,4 +2,5 @@ package io.github.trimax.venta.engine.model.common.scene;
 
 import org.joml.Vector3f;
 
-public record Fog(Vector3f color, float density) {}
+public record Fog(Vector3f color, float density) {
+}
