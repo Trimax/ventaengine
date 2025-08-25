5aefd440b2a0f88182ee52a19c18dee76550a486
Vasili Zvarych <vasili.zvarych@bcdtravel.com>
2025-08-25 19:39:29 +0200

    VE-37: refactoring

--- engine/src/main/java/io/github/trimax/venta/engine/model/common/scene/Fog.java
+++ engine/src/main/java/io/github/trimax/venta/engine/model/common/scene/Fog.java
@@ -1,13 +1,5 @@
 package io.github.trimax.venta.engine.model.common.scene;
 
-import io.github.trimax.venta.engine.enums.FogType;
 import org.joml.Vector3f;
 
-public record Fog(
-        Vector3f color,
-        float density
-) {
-    public static Fog exponential(final Vector3f color, final float density) {
-        return new Fog(color, density);
-    }
-}
+public record Fog(Vector3f color, float density) {}
