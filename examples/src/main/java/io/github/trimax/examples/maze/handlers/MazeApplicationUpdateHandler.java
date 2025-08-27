e1d9bda9b787a8b4d0d745f50b7a5c6febcc7567
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-08-27 18:15:42 +0200

    Maze: movement like in classic FPS

--- examples/src/main/java/io/github/trimax/examples/maze/handlers/MazeApplicationUpdateHandler.java
+++ examples/src/main/java/io/github/trimax/examples/maze/handlers/MazeApplicationUpdateHandler.java
@@ -6,6 +6,7 @@ import io.github.trimax.venta.engine.core.Engine;
 import io.github.trimax.venta.engine.interfaces.VentaEngineUpdateHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.joml.Vector3f;
 
 import static org.lwjgl.glfw.GLFW.*;
 
@@ -14,12 +15,17 @@ import static org.lwjgl.glfw.GLFW.*;
 public final class MazeApplicationUpdateHandler implements VentaEngineUpdateHandler {
     private final MazeApplicationState state;
 
+    private final Vector3f direction = new Vector3f(0f);
+
     public void onUpdate(final Engine.VentaTime time, final VentaContext context) {
+        direction.set(state.getCamera().getFront());
+        direction.y = 0f;
+
         if (context.isButtonPushed(GLFW_KEY_DOWN))
-            state.getCamera().moveForward(-(float) time.getDelta());
+            state.getCamera().move(direction.mul(-(float) time.getDelta()));
 
         if (context.isButtonPushed(GLFW_KEY_UP))
-            state.getCamera().moveForward((float) time.getDelta());
+            state.getCamera().move(direction.mul((float) time.getDelta()));
 
         if (context.isButtonPushed(GLFW_KEY_LEFT))
             state.getCamera().moveRight(-(float) time.getDelta());
