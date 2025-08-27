ba01084dee534b1debb38e84abae706d68a13c17
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-08-27 17:20:54 +0200

    Cubemap loading refactoring

--- engine/src/main/java/io/github/trimax/venta/engine/definitions/GeometryDefinitions.java
+++ engine/src/main/java/io/github/trimax/venta/engine/definitions/GeometryDefinitions.java
@@ -1,16 +1,10 @@
-package io.github.trimax.venta.engine.utils;
+package io.github.trimax.venta.engine.definitions;
 
 import lombok.experimental.UtilityClass;
 
-import static org.lwjgl.opengl.GL15C.*;
-import static org.lwjgl.opengl.GL20C.glEnableVertexAttribArray;
-import static org.lwjgl.opengl.GL20C.glVertexAttribPointer;
-import static org.lwjgl.opengl.GL30C.*;
-
-//TODO: Reimplement using encapsulation
 @UtilityClass
-public final class SkyboxUtil {
-    private static final float[] SKYBOX_VERTICES = {
+public final class GeometryDefinitions {
+    public static final float[] SKYBOX_VERTICES = {
             -1.0f,  1.0f, -1.0f,
             -1.0f, -1.0f, -1.0f,
             1.0f, -1.0f, -1.0f,
@@ -54,29 +48,9 @@ public final class SkyboxUtil {
             1.0f, -1.0f,  1.0f
     };
 
-    public int[] create() {
-        final var vao = glGenVertexArrays();
-        final var vbo = glGenBuffers();
-
-        glBindVertexArray(vao);
-
-        glBindBuffer(GL_ARRAY_BUFFER, vbo);
-        glBufferData(GL_ARRAY_BUFFER, SKYBOX_VERTICES, GL_STATIC_DRAW);
-
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, false, 3 * Float.BYTES, 0L);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-
-        return new int[]{vao, vbo};
-    }
-
-    public void destroySkyboxVAO(final int vao, final int vbo) {
-        if (vbo != 0)
-            glDeleteBuffers(vbo);
-
-        if (vao != 0)
-            glDeleteVertexArrays(vao);
-    }
+    public static final float[] CONSOLE_VERTICES = {
+            /* Top-left */    -1.0f, 1.0f,
+            /* Top-right */    1.0f, 1.0f,
+            /* Bottom-right */ 1.0f, 0.0f,
+            /* Bottom-left */ -1.0f, 0.0f};
 }
