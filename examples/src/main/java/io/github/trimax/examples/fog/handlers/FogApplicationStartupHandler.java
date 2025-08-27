e99566fe1392441672b38e2b553a73b915311b1b
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-08-27 17:39:36 +0200

    Cleaning up fog demo application

--- examples/src/main/java/io/github/trimax/examples/fog/handlers/FogApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/fog/handlers/FogApplicationStartupHandler.java
@@ -7,24 +7,25 @@ import io.github.trimax.venta.engine.model.common.scene.Fog;
 import lombok.AllArgsConstructor;
 import org.joml.Vector3f;
 
+import static io.github.trimax.examples.fog.definitions.Definitions.CIRCLE_RADIUS;
+import static io.github.trimax.examples.fog.definitions.Definitions.CUBE_COUNT;
+
 @AllArgsConstructor
 public final class FogApplicationStartupHandler implements VentaEngineStartupHandler {
-    private static final int CUBE_COUNT = 10;
-    private static final float CIRCLE_RADIUS = 10.0f;
     private final FogApplicationState state;
 
     public void onStartup(final String[] args, final VentaContext context) {
         final var scene = context.getSceneManager().getCurrent();
-        scene.setSkybox(context.getCubemapRegistry().get("stars.json"));
+        scene.setSkybox(context.getCubemapRegistry().get("debug.json"));
         scene.setAmbientLight(new Vector3f(0.8f, 0.8f, 0.8f));
 
         scene.setFog(new Fog(10.f, 25.f, new Vector3f(0.6f, 0.6f, 0.6f)));
+        state.setFog(scene.getFog());
 
         final var camera = context.getCameraManager().getCurrent();
         camera.setPosition(new Vector3f(-17.0f, 4.0f, 0.0f));
         camera.lookAt(new Vector3f(0.f, 4.f, 0.f));
         state.setCamera(camera);
-        state.setFog(scene.getFog());
 
         for (int cubeID = 0; cubeID < CUBE_COUNT; cubeID++) {
             final var cube = context.getObjectManager().create("cube" + cubeID,
