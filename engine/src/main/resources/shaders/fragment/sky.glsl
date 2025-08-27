fb76daf231a4689f29ec994eef3a8bdd1b8ecbaf
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-08-27 14:14:11 +0200

    Skybox with fog

--- engine/src/main/resources/shaders/fragment/sky.glsl
+++ engine/src/main/resources/shaders/fragment/sky.glsl
@@ -1,14 +1,31 @@
 #version 330 core
 
+struct Fog {
+    vec3 color;
+    float minimalDistance;
+    float maximalDistance;
+};
+
 /* Vertex shader output */
 in vec3 vertexPosition;
 
 /* Skybox texture */
 uniform samplerCube skybox;
 
+/* Fog */
+uniform Fog fog;
+
+/* Feature flags */
+uniform int useFog;
+
 /* Output color */
 out vec4 FragColor;
 
+/* Checks if flag is set (bools are not supported by macOS Radeon cards */
+bool isSet(int value) {
+    return value != 0;
+}
+
 void main() {
-    FragColor = texture(skybox, vertexPosition);
+    FragColor = isSet(useFog) ? vec4(fog.color, 1.0) : texture(skybox, vertexPosition);
 }
