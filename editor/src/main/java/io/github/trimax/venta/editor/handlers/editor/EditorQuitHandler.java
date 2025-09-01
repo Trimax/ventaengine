e25f2b06b589bb5b465c47d2387c61f1d32fc204
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-09-01 18:05:56 +0200

    JavaFX app quit handler fix

--- editor/src/main/java/io/github/trimax/venta/editor/handlers/editor/EditorQuitHandler.java
+++ editor/src/main/java/io/github/trimax/venta/editor/handlers/editor/EditorQuitHandler.java
@@ -4,10 +4,17 @@ import io.github.trimax.venta.editor.utils.DialogUtil;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.stage.Window;
+import one.util.streamex.StreamEx;
 
 public final class EditorQuitHandler implements EventHandler<ActionEvent> {
     @Override
     public void handle(final ActionEvent event) {
-        DialogUtil.showConfirm("Are you sure you want to close an editor? All unsaved changes will be lost.", Platform::exit);
+        DialogUtil.showConfirm("Are you sure you want to close an editor? All unsaved changes will be lost.", this::exit);
+    }
+
+    private void exit() {
+        StreamEx.of(Window.getWindows()).forEach(Window::hide);
+        Platform.runLater(Platform::exit);
     }
 }
