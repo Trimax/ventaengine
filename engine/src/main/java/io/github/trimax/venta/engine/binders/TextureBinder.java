be05f79918e1ea4d9b9a66c15395882bbd69df49
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-09-02 20:01:04 +0200

    Texture binder created

---
+++ engine/src/main/java/io/github/trimax/venta/engine/binders/TextureBinder.java
@@ -0,0 +1,33 @@
+package io.github.trimax.venta.engine.binders;
+
+import io.github.trimax.venta.container.annotations.Component;
+import io.github.trimax.venta.engine.enums.ShaderUniform;
+import io.github.trimax.venta.engine.model.entity.implementation.CubemapEntityImplementation;
+import io.github.trimax.venta.engine.model.entity.implementation.ProgramEntityImplementation;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import static org.lwjgl.opengl.GL11C.glBindTexture;
+import static org.lwjgl.opengl.GL13C.*;
+import static org.lwjgl.opengl.GL20C.glUniform1i;
+
+@Slf4j
+@Component
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TextureBinder extends AbstractBinder {
+    public void bind(final ProgramEntityImplementation program, final CubemapEntityImplementation skybox) {
+        bind(program.getUniformID(ShaderUniform.UseTextureSkybox), skybox != null);
+
+        glActiveTexture(GL_TEXTURE0);
+        if (skybox == null) {
+            glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+            glUniform1i(program.getUniformID(ShaderUniform.UseTextureSkybox), 0);
+            return;
+        }
+
+        glBindTexture(GL_TEXTURE_CUBE_MAP, skybox.getInternalID());
+        glUniform1i(program.getUniformID(ShaderUniform.TextureSkybox), 0);
+        glUniform1i(program.getUniformID(ShaderUniform.UseTextureSkybox), 1);
+    }
+}
--- engine/src/main/java/io/github/trimax/venta/engine/enums/TextureType.java
+++ engine/src/main/java/io/github/trimax/venta/engine/enums/TextureType.java
@@ -9,13 +9,13 @@ import static org.lwjgl.opengl.GL13C.*;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum TextureType {
-    AmbientOcclusion("ambientOcclusion", GL_TEXTURE3, 3),
-    Anisotropy("anisotropy", GL_TEXTURE6, 6),
-    Diffuse("diffuse", GL_TEXTURE0, 0),
-    Height("height", GL_TEXTURE1, 1),
-    Metalness("metalness", GL_TEXTURE5, 5),
-    Normal("normal", GL_TEXTURE2, 2),
-    Roughness("roughness", GL_TEXTURE4, 4);
+    AmbientOcclusion("ambientOcclusion", GL_TEXTURE4, 4),
+    Anisotropy("anisotropy", GL_TEXTURE7, 7),
+    Diffuse("diffuse", GL_TEXTURE1, 1),
+    Height("height", GL_TEXTURE2, 2),
+    Metalness("metalness", GL_TEXTURE6, 6),
+    Normal("normal", GL_TEXTURE3, 3),
+    Roughness("roughness", GL_TEXTURE5, 5);
 
     private final String fieldName;
     private final int locationID;
