29d483397e9659addc8db591bc8259c9734d161f
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-09-08 10:24:51 +0200

    Default shader lighting model improvement

--- engine/src/main/resources/shaders/default/fragment.glsl
+++ engine/src/main/resources/shaders/default/fragment.glsl
@@ -4,6 +4,10 @@
  * Definitions
  ***/
 
+/* Common */
+const float METALNESS_MINIMAL = 0.04;
+
+/* Light specific */
 const int MAX_LIGHTS = 64;
 const int LIGHT_TYPE_POINT = 0;
 const int LIGHT_TYPE_DIRECTIONAL = 1;
@@ -178,43 +182,50 @@ float getAttenuation(Attenuation attenuation, float distance) {
     return 1.0 / (attenuation.constant + attenuation.linear * distance + attenuation.quadratic * distance * distance);
 }
 
+/* Calculates diffuse light component */
+vec3 calculateLightDiffuse(vec3 lightColor, vec3 normal, vec3 lightDirection) {
+    return max(dot(normal, lightDirection), 0.0) * lightColor;
+}
+
+/* Calculates specular light component */
+vec3 calculateLightSpecular(vec4 color, vec3 lightColor, vec3 normal, vec3 lightDirection, vec3 viewDirection, vec2 textureCoordinates) {
+    vec3 F0 = mix(vec3(METALNESS_MINIMAL), color.rgb, getMetalness(textureCoordinates));
+    float shininess = mix(1.0, 256.0, 1.0 - getRoughness(textureCoordinates));
+    float factorSpecular = pow(max(dot(vertexViewDirectionWorldSpace, reflect(-lightDirection, normal)), 0.0), shininess);
+
+    return F0 * factorSpecular * lightColor;
+}
+
 /* Calculates effect of one light source */
-vec3 calculateLight(Light light, vec3 normal, vec3 fragmentPosition, vec2 textureCoordinates) {
+vec3 calculateLight(vec4 color, Light light, vec3 normal, vec3 fragmentPosition, vec2 textureCoordinates) {
     if (!isSet(light.enabled))
         return vec3(0.0);
 
-    vec3 lightDir = vec3(0.0);
+    vec3 lightDirection = vec3(0.0);
     float attenuation = 1.0;
 
     switch (light.type) {
         case LIGHT_TYPE_POINT:
         case LIGHT_TYPE_SPOT:
-            lightDir = normalize(light.position - fragmentPosition);
+            lightDirection = normalize(light.position - fragmentPosition);
             attenuation = getAttenuation(light.attenuation, length(light.position - fragmentPosition));
             break;
 
         case LIGHT_TYPE_DIRECTIONAL:
-            lightDir = normalize(-light.direction);
+            lightDirection = normalize(-light.direction);
             break;
 
         default:
             return vec3(0.0);
     }
 
-    float factorDiffuse = max(dot(normal, lightDir), 0.0);
-    vec3 colorDiffuse = factorDiffuse * light.color;
-
-    //
-
-    //TODO: Compute specular instnsity (0.5) based on metalness texture (float dielectricF0 = 0.04; vec3 F0 = mix(vec3(dielectricF0), getMaterialColor().rgb, getMetalness(texCoords));)
-    float shininess = mix(1.0, 256.0, 1.0 - getRoughness(textureCoordinates));
-    float factorSpecular = pow(max(dot(vertexViewDirectionWorldSpace, reflect(-lightDir, normal)), 0.0), shininess);
-    vec3 colorSpecular = 0.5 * factorSpecular * light.color;
+    vec3 colorDiffuse = calculateLightDiffuse(light.color, normal, lightDirection);
+    vec3 colorSpecular = calculateLightSpecular(color, light.color, normal, lightDirection, vertexViewDirectionWorldSpace, textureCoordinates);
 
     return (colorDiffuse + colorSpecular) * light.intensity * attenuation;
 }
 
-vec3 calculateLighting(vec2 textureCoordinates) {
+vec3 calculateLighting(vec4 color, vec2 textureCoordinates) {
     if (!isSet(useLighting))
         return vec3(1.0);
 
@@ -222,13 +233,13 @@ vec3 calculateLighting(vec2 textureCoordinates) {
 
     vec3 lighting = ambientLight.xyz;
     for (int i = 0; i < lightCount; i++)
-        lighting += calculateLight(lights[i], normal, vertexPosition, textureCoordinates);
+        lighting += calculateLight(color, lights[i], normal, vertexPosition, textureCoordinates);
 
     return lighting;
 }
 
 vec4 applyLighting(vec4 color, vec2 textureCoordinates) {
-    vec3 lighting = calculateLighting(textureCoordinates) * getAmbientOcclusion(textureCoordinates);
+    vec3 lighting = calculateLighting(color, textureCoordinates) * getAmbientOcclusion(textureCoordinates);
 
     return vec4(clamp(color.rgb * lighting, 0.0, 1.0), color.a);
 }
