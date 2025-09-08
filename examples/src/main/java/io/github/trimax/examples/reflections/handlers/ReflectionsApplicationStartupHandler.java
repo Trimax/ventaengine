d2af69e8d1afa8b38dcd85a93eb0557f4a2bfaca
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-09-08 10:51:03 +0200

    Reflection example extended with rust metal material

--- examples/src/main/java/io/github/trimax/examples/reflections/handlers/ReflectionsApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/reflections/handlers/ReflectionsApplicationStartupHandler.java
@@ -24,13 +24,18 @@ public final class ReflectionsApplicationStartupHandler implements VentaEngineSt
         cubeMirror.setMaterial(context.getMaterialRegistry().get("mirror.json"));
         scene.add(cubeMirror);
 
+        final var cubeRust = context.getObjectManager().create("Cube like metal", context.getObjectRepository().get("cube.json"));
+        cubeRust.setPosition(new Vector3f(0.f, -1.f, 0.f));
+        cubeRust.setMaterial(context.getMaterialRegistry().get("rust.json"));
+        scene.add(cubeRust);
+
         final var cubeMetal = context.getObjectManager().create("Cube like metal", context.getObjectRepository().get("cube.json"));
-        cubeMetal.setPosition(new Vector3f(0.f, -1.f, 0.f));
+        cubeMetal.setPosition(new Vector3f(0.f, -2.f, 0.f));
         cubeMetal.setMaterial(context.getMaterialRegistry().get("metal.json"));
         scene.add(cubeMetal);
 
         final var cube = context.getObjectManager().create("Cube", context.getObjectRepository().get("cube.json"));
-        cube.setPosition(new Vector3f(0.f, -2.f, 0.f));
+        cube.setPosition(new Vector3f(0.f, -3.f, 0.f));
         scene.add(cube);
 
         final var camera = context.getCameraManager().getCurrent();
