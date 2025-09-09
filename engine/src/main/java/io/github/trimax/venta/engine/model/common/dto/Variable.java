951e66138937400a10551098243c5ee21ba52528
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-09-09 14:32:14 +0200

    Checkstyle

--- engine/src/main/java/io/github/trimax/venta/engine/model/common/dto/Variable.java
+++ engine/src/main/java/io/github/trimax/venta/engine/model/common/dto/Variable.java
@@ -2,5 +2,5 @@ package io.github.trimax.venta.engine.model.common.dto;
 
 import lombok.NonNull;
 
-public record Variable<T> (@NonNull T value, @NonNull T deviation) {
+public record Variable<T>(@NonNull T value, @NonNull T deviation) {
 }
