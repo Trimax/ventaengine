23137849298b14b17f2af815505c156706155b61
Vasili Zvarych <vasili.zvarych@bcdtravel.com>
2025-09-12 10:35:09 +0200

    VE-43: unused import

--- examples/src/main/java/io/github/trimax/examples/sound/handlers/SoundDemoApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/sound/handlers/SoundDemoApplicationStartupHandler.java
@@ -2,7 +2,6 @@ package io.github.trimax.examples.sound.handlers;
 
 import io.github.trimax.venta.engine.context.VentaContext;
 import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
-import io.github.trimax.venta.engine.model.entity.SoundEntity;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
