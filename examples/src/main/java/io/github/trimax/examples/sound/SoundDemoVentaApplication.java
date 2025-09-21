f8d38d7f7a7f55e11787120401948faad1dd7aee
Vasili Zvarych <vasilii.zvarich@gmail.com>
2025-09-21 14:40:48 +0200

    Gizmo (#101)
    Co-authored-by: Vasili Zvarych <vasili.zvarych@bcdtravel.com>

--- examples/src/main/java/io/github/trimax/examples/sound/SoundDemoVentaApplication.java
+++ examples/src/main/java/io/github/trimax/examples/sound/SoundDemoVentaApplication.java
@@ -2,11 +2,23 @@ package io.github.trimax.examples.sound;
 
 import io.github.trimax.examples.sound.handlers.SoundDemoApplicationStartupHandler;
 import io.github.trimax.venta.engine.VentaEngine;
+import io.github.trimax.venta.engine.enums.AntialiasingSamples;
 import io.github.trimax.venta.engine.interfaces.VentaEngineApplication;
+import io.github.trimax.venta.engine.interfaces.VentaEngineConfiguration;
 import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
 import lombok.NonNull;
 
 public final class SoundDemoVentaApplication implements VentaEngineApplication {
+    @Override
+    public @NonNull VentaEngineConfiguration getConfiguration() {
+        return new VentaEngineConfiguration() {
+            @Override
+            public @NonNull RenderConfiguration getRenderConfiguration() {
+                return new RenderConfiguration(false, true, true, false, AntialiasingSamples.X2);
+            }
+        };
+    }
+
     @Override
     public @NonNull VentaEngineStartupHandler getStartupHandler() {
         return new SoundDemoApplicationStartupHandler();
--- examples/src/main/java/io/github/trimax/examples/sound/handlers/SoundDemoApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/sound/handlers/SoundDemoApplicationStartupHandler.java
@@ -1,16 +1,20 @@
 package io.github.trimax.examples.sound.handlers;
 
-import org.joml.Vector3f;
-
 import io.github.trimax.venta.engine.context.VentaContext;
 import io.github.trimax.venta.engine.interfaces.VentaEngineStartupHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.joml.Vector3f;
 
 @Slf4j
 public final class SoundDemoApplicationStartupHandler implements VentaEngineStartupHandler {
     @Override
     public void onStartup(final String[] args, final VentaContext context) {
         final var scene = context.getSceneManager().getCurrent();
+        scene.setAmbientLight(new Vector3f(0.8f, 0.8f, 0.8f));
+
+        final var camera = context.getCameraManager().getCurrent();
+        camera.setPosition(new Vector3f(5.f, 5.f, 5.f));
+        camera.lookAt(new Vector3f(0.f));
 
         final var sound1 = context.getSoundSourceManager().create("engine1", context.getSoundSourceRepository().get("default.json"));
         sound1.setPosition(new Vector3f(1.0f, 0.0f, 0.0f));  
