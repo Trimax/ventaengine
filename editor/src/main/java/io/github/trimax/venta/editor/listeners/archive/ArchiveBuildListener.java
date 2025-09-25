4c96effb1e6fdd117a0fd6c7f40220f04f673b71
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-09-25 12:52:37 +0200

    Editor: building archive

--- editor/src/main/java/io/github/trimax/venta/editor/listeners/archive/ArchiveBuildListener.java
+++ editor/src/main/java/io/github/trimax/venta/editor/listeners/archive/ArchiveBuildListener.java
@@ -3,10 +3,12 @@ package io.github.trimax.venta.editor.listeners.archive;
 import com.google.common.eventbus.Subscribe;
 import io.github.trimax.venta.container.annotations.Component;
 import io.github.trimax.venta.container.utils.EventUtil;
+import io.github.trimax.venta.editor.context.Context;
 import io.github.trimax.venta.editor.controllers.TreeController;
 import io.github.trimax.venta.editor.listeners.AbstractListener;
 import io.github.trimax.venta.editor.model.event.archive.ArchiveBuildEvent;
 import io.github.trimax.venta.editor.model.event.status.StatusSetEvent;
+import io.github.trimax.venta.engine.enums.GroupType;
 import io.github.trimax.venta.engine.enums.ResourceType;
 import io.github.trimax.venta.engine.model.common.resource.Item;
 import javafx.scene.control.TreeItem;
@@ -14,6 +16,7 @@ import lombok.AccessLevel;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import one.util.streamex.StreamEx;
 
 import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
@@ -26,20 +29,29 @@ import java.util.zip.GZIPOutputStream;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ArchiveBuildListener implements AbstractListener<ArchiveBuildEvent> {
     private final TreeController treeController;
+    private final Context context;
 
     @Override
     @Subscribe
     @SneakyThrows
     public void handle(@NonNull final ArchiveBuildEvent event) {
+        context.setTree(context.getGroupSelected(), treeController.getRoot());
+
         try (final var out = new DataOutputStream(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(event.file()))))) {
-            writeNode(out, treeController.getRoot(), null);
+            StreamEx.of(GroupType.values()).forEach(group -> writeGroup(out, group));
         }
 
         EventUtil.post(new StatusSetEvent("Archive built and saved to %s", event.file().getAbsoluteFile()));
     }
 
     @SneakyThrows
-    private void writeNode(final DataOutputStream out, final TreeItem<Item> node, final String type) {
+    private void writeGroup(@NonNull final DataOutputStream out, @NonNull final GroupType type) {
+        out.writeUTF(type.name());
+        writeNode(out, context.getTree(type), null);
+    }
+
+    @SneakyThrows
+    private void writeNode(@NonNull final DataOutputStream out, final TreeItem<Item> node, final String type) {
         final var item = node.getValue();
         out.writeUTF(item.name().toLowerCase());
 
