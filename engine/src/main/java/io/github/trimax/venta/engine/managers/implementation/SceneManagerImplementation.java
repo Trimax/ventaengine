167c36d0fc7f7194eadcac48255092fd2247abe3
Dmitrii Eliseev <759263+Trimax@users.noreply.github.com>
2025-09-26 13:02:46 +0200

    Scene instantiation refactoring

--- engine/src/main/java/io/github/trimax/venta/engine/managers/implementation/SceneManagerImplementation.java
+++ engine/src/main/java/io/github/trimax/venta/engine/managers/implementation/SceneManagerImplementation.java
@@ -10,20 +10,22 @@ import io.github.trimax.venta.engine.model.common.scene.SceneEmitter;
 import io.github.trimax.venta.engine.model.common.scene.SceneLight;
 import io.github.trimax.venta.engine.model.common.scene.SceneObject;
 import io.github.trimax.venta.engine.model.common.scene.SceneSoundSource;
+import io.github.trimax.venta.engine.model.instance.BillboardInstance;
+import io.github.trimax.venta.engine.model.instance.ObjectInstance;
 import io.github.trimax.venta.engine.model.instance.SceneInstance;
 import io.github.trimax.venta.engine.model.instance.implementation.Abettor;
+import io.github.trimax.venta.engine.model.instance.implementation.EmitterInstanceImplementation;
+import io.github.trimax.venta.engine.model.instance.implementation.LightInstanceImplementation;
 import io.github.trimax.venta.engine.model.instance.implementation.SceneInstanceImplementation;
 import io.github.trimax.venta.engine.model.instance.implementation.SoundSourceInstanceImplementation;
 import io.github.trimax.venta.engine.model.prefabs.ScenePrefab;
 import io.github.trimax.venta.engine.model.prefabs.implementation.ScenePrefabImplementation;
-import io.github.trimax.venta.engine.repositories.implementation.BillboardRepositoryImplementation;
-import io.github.trimax.venta.engine.repositories.implementation.EmitterRepositoryImplementation;
-import io.github.trimax.venta.engine.repositories.implementation.SoundSourceRepositoryImplementation;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import one.util.streamex.StreamEx;
 
 @Slf4j
 @Component
@@ -31,10 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 public final class SceneManagerImplementation
         extends AbstractManagerImplementation<SceneInstanceImplementation, SceneInstance>
         implements SceneManager {
-    private final SoundSourceRepositoryImplementation soundSourceRepository;
-    private final BillboardRepositoryImplementation billboardRepository;
     private final SoundSourceManagerImplementation soundSourceManager;
-    private final EmitterRepositoryImplementation emitterRepository;
     private final BillboardManagerImplementation billboardManager;
     private final EmitterManagerImplementation emitterManager;
     private final ObjectManagerImplementation objectManager;
@@ -52,47 +51,22 @@ public final class SceneManagerImplementation
         throw new UnknownInstanceException(prefab.getClass());
     }
 
+    @Override
+    public SceneInstanceImplementation create(@NonNull final String name) {
+        log.info("Creating scene {}", name);
+
+        return store(abettor.createScene(name));
+    }
+
     private SceneInstanceImplementation create(@NonNull final String name, @NonNull final ScenePrefabImplementation prefab) {
         final var scene = create(name);
 
-        for (final var objectPrefab : prefab.getObjects()) {
-            final var object = objectManager.create(String.format("%s-%s", name, objectPrefab.getName()), objectPrefab.getPrefab());
-            Optional.of(objectPrefab).map(SceneObject::getPosition).ifPresent(object::setPosition);
-            Optional.of(objectPrefab).map(SceneObject::getAngles).ifPresent(object::setRotation);
-            Optional.of(objectPrefab).map(SceneObject::getScale).ifPresent(object::setScale);
+        StreamEx.of(prefab.getObjects()).map(this::createObject).forEach(scene::add);
+        StreamEx.of(prefab.getLights()).map(this::createLight).forEach(scene::add);
+        StreamEx.of(prefab.getEmitters()).map(this::createEmitter).forEach(scene::add);
+        StreamEx.of(prefab.getBillboards()).map(this::createBillboard).forEach(scene::add);
+        StreamEx.of(prefab.getSoundSources()).map(this::createSoundSource).forEach(scene::add);
 
-            scene.add(object);
-        }
-
-        for (final var sceneLight : prefab.getLights()) {
-            final var light = lightManager.create(String.format("%s-%s", name, sceneLight.getName()), sceneLight.getPrefab());
-            Optional.of(sceneLight).map(SceneLight::getPosition).ifPresent(light::setPosition);
-            Optional.of(sceneLight).map(SceneLight::getDirection).ifPresent(light::setDirection);
-
-            scene.add(light);
-        }
-
-        for (final var emitterPrefab : prefab.getEmitters()) {
-            final var emitter = emitterManager.create(String.format("%s-%s", name, emitterPrefab.getName()), emitterPrefab.getPrefab());
-            Optional.of(emitterPrefab).map(SceneEmitter::getPosition).ifPresent(emitter::setPosition);
-
-            scene.add(emitter);
-        }
-
-        for (final var dto : prefab.getBillboards()) {
-            final var billboard = billboardManager.create(String.format("%s-%s", name, dto.getName()), dto.getPrefab());
-            Optional.of(dto).map(SceneBillboard::getPosition).ifPresent(billboard::setPosition);
-            Optional.of(dto).map(SceneBillboard::getSize).ifPresent(billboard::setSize);
-
-            scene.add(billboard);
-        }
-
-        for (final var dto : prefab.getSoundSources()) {
-            final var soundSource = soundSourceManager.create(String.format("%s-%s", name, dto.getName()), dto.getPrefab());
-            Optional.of(dto).map(SceneSoundSource::getPosition).ifPresent(soundSource::setPosition);
-
-            scene.add(soundSource);
-        }
         Optional.ofNullable(prefab.getAmbientLight()).ifPresent(scene::setAmbientLight);
         Optional.ofNullable(prefab.getFog()).ifPresent(scene::setFog);
         Optional.ofNullable(prefab.getSkybox()).ifPresent(scene::setSkybox);
@@ -100,12 +74,46 @@ public final class SceneManagerImplementation
         return store(scene);
     }
 
-    @Override
-    public SceneInstanceImplementation create(@NonNull final String name) {
-        log.info("Creating scene {}", name);
+    private SoundSourceInstanceImplementation createSoundSource(@NonNull final SceneSoundSource sceneSoundSource) {
+        final var soundSource = soundSourceManager.create(sceneSoundSource.getName(), sceneSoundSource.getPrefab());
+        Optional.of(sceneSoundSource).map(SceneSoundSource::getPosition).ifPresent(soundSource::setPosition);
 
-        return store(abettor.createScene(name));
+        return soundSource;
     }
+
+    private BillboardInstance createBillboard(@NonNull final SceneBillboard sceneBillboard) {
+        final var billboard = billboardManager.create(sceneBillboard.getName(), sceneBillboard.getPrefab());
+        Optional.of(sceneBillboard).map(SceneBillboard::getPosition).ifPresent(billboard::setPosition);
+        Optional.of(sceneBillboard).map(SceneBillboard::getSize).ifPresent(billboard::setSize);
+
+        return billboard;
+    }
+
+    private EmitterInstanceImplementation createEmitter(@NonNull final SceneEmitter sceneEmitter) {
+        final var emitter = emitterManager.create(sceneEmitter.getName(), sceneEmitter.getPrefab());
+        Optional.of(sceneEmitter).map(SceneEmitter::getPosition).ifPresent(emitter::setPosition);
+
+        return emitter;
+    }
+
+    private ObjectInstance createObject(@NonNull final SceneObject sceneObject) {
+        final var object = objectManager.create(sceneObject.getName(), sceneObject.getPrefab());
+        Optional.of(sceneObject).map(SceneObject::getPosition).ifPresent(object::setPosition);
+        Optional.of(sceneObject).map(SceneObject::getAngles).ifPresent(object::setRotation);
+        Optional.of(sceneObject).map(SceneObject::getScale).ifPresent(object::setScale);
+
+        return object;
+    }
+
+    private LightInstanceImplementation createLight(@NonNull final SceneLight sceneLight) {
+        final var light = lightManager.create(sceneLight.getName(), sceneLight.getPrefab());
+        Optional.of(sceneLight).map(SceneLight::getPosition).ifPresent(light::setPosition);
+        Optional.of(sceneLight).map(SceneLight::getDirection).ifPresent(light::setDirection);
+
+        return light;
+    }
+
+
 
     @Override
     public void setCurrent(@NonNull final SceneInstance scene) {
