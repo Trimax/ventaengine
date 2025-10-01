398c844b980fc2e5e5d73d0a1bcdaa059797ef73
Dmitrii Eliseev <eliseev.dmitry@gmail.com>
2025-10-01 10:19:42 +0200

    Window management & tools handler

---
+++ editor/src/main/java/io/github/trimax/venta/editor/enums/Layout.java
@@ -0,0 +1,16 @@
+package io.github.trimax.venta.editor.enums;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Layout {
+    Main("Venta Editor", "/layouts/main.fxml"),
+    Mixer("Texture mixer", "/tools/mixer/main.fxml"),
+    Generator("Heightmap Generator", "/tools/generator/main.fxml");
+
+    private final String title;
+    private final String path;
+}
---
+++ editor/src/main/java/io/github/trimax/venta/editor/utils/WindowUtil.java
@@ -0,0 +1,53 @@
+package io.github.trimax.venta.editor.utils;
+
+import io.github.trimax.venta.container.VentaApplication;
+import io.github.trimax.venta.editor.VentaEditor;
+import io.github.trimax.venta.editor.enums.Layout;
+import io.github.trimax.venta.engine.definitions.DefinitionsIcon;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+import java.io.IOException;
+import java.util.Objects;
+
+@UtilityClass
+public final class WindowUtil {
+    @SneakyThrows
+    public void create(@NonNull final Layout layout, @NonNull final Stage stage) {
+        final var scene = getScene(layout);
+
+        stage.getIcons().add(new Image(Objects.requireNonNull(WindowUtil.class.getResourceAsStream(DefinitionsIcon.DEFAULT))));
+        stage.setTitle(layout.getTitle());
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @SneakyThrows
+    public void createModal(@NonNull final Layout layout) {
+        final var scene = getScene(layout);
+
+        final var stage = new Stage();
+        stage.getIcons().add(new Image(Objects.requireNonNull(WindowUtil.class.getResourceAsStream(DefinitionsIcon.DEFAULT))));
+        stage.setTitle(layout.getTitle());
+        stage.setScene(scene);
+
+        stage.initModality(Modality.WINDOW_MODAL);
+        stage.initOwner(VentaEditor.getStage());
+
+        stage.showAndWait();
+    }
+
+    private Scene getScene(@NonNull final Layout layout) throws IOException {
+        final var loader = new FXMLLoader(WindowUtil.class.getResource(layout.getPath()));
+        loader.setControllerFactory(VentaApplication::getComponent);
+
+        return new Scene(loader.load(), 800, 600);
+    }
+}
