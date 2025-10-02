63d76cbaa710e8ead2391911e5ef3fc266104d64
Vasili Zvarych <vasilii.zvarich@gmail.com>
2025-10-02 21:11:44 +0200

    archive loading fix (#132)
    Co-authored-by: Vasili Zvarych <vasili.zvarych@bcdtravel.com>

--- engine/src/main/java/io/github/trimax/venta/engine/utils/ArchiveUtil.java
+++ engine/src/main/java/io/github/trimax/venta/engine/utils/ArchiveUtil.java
@@ -8,10 +8,7 @@ import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.TriConsumer;
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.GZIPInputStream;
@@ -32,23 +29,23 @@ public final class ArchiveUtil {
     @SneakyThrows
     private void load(@NonNull final File archive, @NonNull final TriConsumer<String, GroupType, byte[]> resourceLoader) {
         try (var in = new DataInputStream(new GZIPInputStream(new BufferedInputStream(new FileInputStream(archive))))) {
-            loadNode(in, resourceLoader, "");
+            while (in.available() > 0)
+                loadNode(in, resourceLoader, "", GroupType.of(in.readUTF()));
         }
     }
 
     @SneakyThrows
-    private void loadNode(final DataInputStream in, final TriConsumer<String, GroupType, byte[]> resourceLoader, @NonNull final String path) {
+    private void loadNode(final DataInputStream in, final TriConsumer<String, GroupType, byte[]> resourceLoader, @NonNull final String path, final GroupType type) {
         final var name = in.readUTF();
-        final var type = GroupType.of(in.readUTF());
 
         final var hasReference = in.readBoolean();
         if (hasReference) {
             final var length = in.readInt();
-            resourceLoader.accept(String.format("%s/%s", path, name).substring("/Root".length()), type, in.readNBytes(length));
+            resourceLoader.accept(String.format("%s/%s", path, name), type, in.readNBytes(length));
         }
 
         final var childrenCount = in.readInt();
         for (int i = 0; i < childrenCount; i++)
-            loadNode(in, resourceLoader, String.format("%s/%s", path, name));
+            loadNode(in, resourceLoader, String.format("%s/%s", path, name), type);
     }
 }
--- examples/src/main/java/io/github/trimax/examples/archive/handlers/ArchiveApplicationStartupHandler.java
+++ examples/src/main/java/io/github/trimax/examples/archive/handlers/ArchiveApplicationStartupHandler.java
@@ -14,18 +14,7 @@ public final class ArchiveApplicationStartupHandler implements VentaEngineStartu
         final var scene = context.getSceneManager().getCurrent();
         scene.setAmbientLight(new Vector3f(0.f, 0.f, 0.15f));
         context.registerArchive(Paths.get("examples", "data", "example.vea").toString());
-
-        final var cube = context.getObjectManager().create("cube", context.getObjectRepository().get("cube-without-material.json"));
-        cube.setScale(new Vector3f(5.f));
-        scene.add(cube);
-
-        final var lightXZ = context.getLightManager().create("Y light", context.getLightRepository().get("point.json"));
-        lightXZ.setPosition(new Vector3f(0.f, 5.f, 0.f));
-        lightXZ.setIntensity(1.0f);
-        scene.add(lightXZ);
-
-        final var camera = context.getCameraManager().getCurrent();
-        camera.setPosition(new Vector3f(5.f, 5.f, 5.f));
-        camera.lookAt(new Vector3f(0.f));
+        context.getTextureRegistry().get("texture.png");
+        context.getTextureRegistry().get("group/texture.png");
     }
 }
