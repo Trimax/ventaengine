db7dffbdf527e51af9f121552a9afd68610dad32
Vasili Zvarych <vasili.zvarych@bcdtravel.com>
2025-12-02 17:55:30 +0100

    final version

--- editor/src/main/java/io/github/trimax/venta/editor/controllers/heightmap/HeightmapGeneratorController.java
+++ editor/src/main/java/io/github/trimax/venta/editor/controllers/heightmap/HeightmapGeneratorController.java
@@ -5,10 +5,9 @@ import io.github.trimax.venta.container.utils.EventUtil;
 import io.github.trimax.venta.editor.context.HeightmapContext;
 import io.github.trimax.venta.editor.handlers.heightmap.HeightmapSaveHandler;
 import io.github.trimax.venta.editor.model.event.heightmap.HeightmapGenerateEvent;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.InputEvent;
@@ -20,45 +19,66 @@ import lombok.RequiredArgsConstructor;
 public final class HeightmapGeneratorController {
     private final HeightmapContext context;
     private final HeightmapSaveHandler saveHandler;
-    
-    @FXML
-    private Spinner<Integer> randomSeedSpinner;
-    @FXML
-    private Spinner<Integer> widthSpinner;
-    @FXML
-    private Spinner<Integer> heightSpinner;
-    @FXML
-    private Spinner<Double> cellSizeSpinner;
-    @FXML
-    private Spinner<Integer> levelsSpinner;
-    @FXML
-    private Spinner<Double> attenuationSpinner;
-    @FXML
-    private CheckBox groovyCheckBox;
-
-    @FXML
-    private Button saveButton;
 
     @FXML
     public ImageView heightmapImageView;
 
     @FXML
-    private Label placeholderLabel;
+    private void onSaveButton(final ActionEvent event){
+        saveHandler.handle(event);
+    }
 
     @FXML
-    private void onSeedChanged(InputEvent event) {
+    public void initialize() {
+        EventUtil.post(new HeightmapGenerateEvent());
+    }
+
+    @FXML
+    private void onSeedChanged(final InputEvent event) {
         Spinner<?> spinnerSeed = (Spinner<?>) event.getSource();
         context.setRandomSeed((int) spinnerSeed.getValue());
         EventUtil.post(new HeightmapGenerateEvent());
     }
 
     @FXML
-    private void onSaveButton(){
-
+    private void onAttenuationChanged(final InputEvent event) {
+        Spinner<?> spinnerAttenuation = (Spinner<?>) event.getSource();
+        context.setAttenuation((double) spinnerAttenuation.getValue());
+        EventUtil.post(new HeightmapGenerateEvent());
     }
 
     @FXML
-    public void initialize() {
+    private void onLevelsChanged(final InputEvent event) {
+        Spinner<?> spinnerLevels = (Spinner<?>) event.getSource();
+        context.setLevels((int) spinnerLevels.getValue());
+        EventUtil.post(new HeightmapGenerateEvent());
+    }
+
+    @FXML
+    public void onGroovyChanged(final ActionEvent event) {
+        CheckBox checkBox = (CheckBox) event.getSource();
+        context.setGroovy(checkBox.isSelected());
+        EventUtil.post(new HeightmapGenerateEvent());        
+    }
+
+    @FXML
+    public void onWidthChanged(final InputEvent event) {
+        Spinner<?> spinnerWidth = (Spinner<?>) event.getSource();
+        context.setWidth((int) spinnerWidth.getValue());
+        EventUtil.post(new HeightmapGenerateEvent());
+    }
+
+    @FXML
+    public void onHeightChanged(final InputEvent event) {
+        Spinner<?> spinnerHeight = (Spinner<?>) event.getSource();
+        context.setHeight((int) spinnerHeight.getValue());
+        EventUtil.post(new HeightmapGenerateEvent());
+    }
+
+    @FXML
+    public void onCellSizeChanged(final InputEvent event) {
+        Spinner<?> spinnerCellSize = (Spinner<?>) event.getSource();
+        context.setCellSize((double) spinnerCellSize.getValue());
         EventUtil.post(new HeightmapGenerateEvent());
     }
 }
